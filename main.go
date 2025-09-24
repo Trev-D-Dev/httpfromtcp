8de89c9e47531b224c7239a25d63891e77630c0c
Trev-D-Dev <dunn.trevor@pm.me>
2025-09-24 12:11:29 -0400

    finished chapter 1 lesson 4

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,21 +14,44 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// bytes will be stored in this
 	fileBytes := make([]byte, 8)
 
+	// keeps track of the current line
+	currentLine := ""
+
+	// offset to keep track of position in file
 	offset := int64(0)
+
+	// loops until io.EOF exits program
 	for {
+
+		// reads from file, if err occurs and isn't io.EOF, logs a fatal error
 		_, err := messagesFile.ReadAt(fileBytes, offset)
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		}
-
-		fmt.Printf("read: %s\n", fileBytes)
+		// converts bytes to a string
+		bytesString := string(fileBytes)
+		// splits string on new line
+		stringArr := strings.Split(bytesString, "\n")
+		// adds first element of array to current line
+		currentLine += stringArr[0]
 
 		if err == io.EOF {
+			fmt.Printf("read: %s\n", currentLine)
 			os.Exit(0)
 		}
 
+		// if array's length is greater than 1, meaning a line ended
+		if len(stringArr) > 1 {
+			// prints line and then resets it to the second array element
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = stringArr[1]
+		}
+
+		// adds to offset and clears byte array
 		offset += 8
+		fileBytes = make([]byte, 8)
 	}
 }
